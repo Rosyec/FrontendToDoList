0ce44acd12ac145abf879f9fc0b005dd6cfdc319
Rosyec Parrado <chechomens@gmail.com>
2026-01-11 16:12:47 -0500

    create env variable for base url

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import { useAuthStore } from "./store/auth-store"
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 })
 
 api.interceptors.request.use((config) => {
